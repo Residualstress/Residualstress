2cd4b1144cda99967ec238c538e9b59ff56c5a3c
Residualstress <92587824+Residualstress@users.noreply.github.com>
2021-12-16 17:25:20 +0800

    Assignment_1 by Weijie Kuang

---
+++ Obj_Viwer/README.md
@@ -0,0 +1,23 @@
+# Introduction
+This a sample example for shwoing 3D obj with pygame, pyopengl.
+
+# Requirements
+```bash
+pip install pygame
+pip install  PyOpenGL PyOpenGL_accelerate
+```
+
+# Demo
+1. main.py
+After you run the main.py, you are asked for inputing the file name of the model
+You can use C on the keyboard to change the color of model randomly
+You can use Mouse to change the size, direction and position of the Obj model
+
+
+# References
+
+[OBJFileLoader](https://www.pygame.org/wiki/OBJFileLoader)
+[BXT-AR-For-Python](https://gitee.com/791529351/BXT-AR4Python)
+[CSDN](https://blog.csdn.net/hongmaodaxia/article/details/78956487)
+
+
---
+++ Obj_Viwer/code/ogl/light.py
@@ -0,0 +1,68 @@
+import numpy as np
+import pygame, OpenGL
+from pygame.locals import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
+from OpenGL.GLUT.freeglut import *
+
+'''
+GLfloat ambient[] = {0.3f, 0.3f, 0.3f, 1.0f};  // 环境强度
+GLfloat diffuse[] = {1.0f, 1.0f, 1.0f, 1.0f};  // 散射强度
+GLfloat specular[] = {1.0f, 1.0f, 1.0f, 1.0f}; // 镜面强度
+
+'''
+def setup_lighting():
+    draw_2side=False
+    c=[1.0, 1.0, 1.0]
+    glColor3fv(c)
+    glClearColor(1.0, 1.0, 1.0, 0.1)
+    glClear(GL_COLOR_BUFFER_BIT)
+
+    mat_specular=[0.18, 0.18, 0.18, 0.18 ]
+
+    mat_shininess=[ 64 ]
+    global_RGB = [ 128, 0, 0]
+    global_ambient = [x/255 for x in global_RGB]+[0.5]
+    light0_ambient=[ 0, 0, 0, 0 ]
+    #light0_ambient = [0.2, 0.2, 0.2, 0.2]
+    #light0_diffuse=[ 0.85, 0.85, 0.8, 0.85 ]
+    light0_diffuse=[ 0.35, 0.35, 0.6, 0.65 ]
+
+    light1_diffuse=[-0.01, -0.01, -0.03, -0.03 ]
+    #light0_specular=[ 0.85, 0.85, 0.85, 0.85 ]
+    light0_specular = [0.25, 0.25, 0.25, 0.25]
+    glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, mat_specular);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, mat_shininess);
+    glLightfv(GL_LIGHT0, GL_AMBIENT, light0_ambient);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, light0_diffuse);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, light0_specular);
+    glLightfv(GL_LIGHT1, GL_DIFFUSE, light1_diffuse);
+    glLightModelfv(GL_LIGHT_MODEL_AMBIENT, global_ambient);
+    glLightModeli(GL_LIGHT_MODEL_LOCAL_VIEWER, GL_FALSE);
+    glLightModeli(GL_LIGHT_MODEL_TWO_SIDE, draw_2side);
+    glEnable(GL_LIGHTING); # 开灯
+    glEnable(GL_LIGHT0);
+    #glEnable(GL_LIGHT1);
+    glDisable(GL_LIGHT1)
+    glEnable(GL_COLOR_MATERIAL);
+    glEnable(GL_NORMALIZE);
+
+def change_lighting( R, G ,B):
+    def random():
+        x = np.random.randint(0, 256)
+        return x
+    R = random()
+    G = random()
+    B = random()
+    global_RGB = [R, G, B]
+    global_ambient = [x / 255 for x in global_RGB] + [0.5]
+    glLightModelfv(GL_LIGHT_MODEL_AMBIENT, global_ambient);
+
+class camera():
+    class Ortho:
+        # left, right, bottom, top, near, far
+        params=np.array([-1, 1, -1, 1, 1, -1], np.float32)
+        bbox=params[0:4]
+        nf=params[4:] # near far
